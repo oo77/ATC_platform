8a37b7b95b04f4b2553d98acb79c89533d753dac
oo77 <54396118+oo77@users.noreply.github.com>
2026-02-20 11:36:55 +0500

    c

--- app/app.vue
+++ app/app.vue
@@ -8,18 +8,16 @@
 </template>
 
 <script setup lang="ts">
-// Инициализация темы на клиенте
-useTheme()
+useTheme();
 
 useHead({
-  titleTemplate: (title) => title 
-    ? `${title} | ATC - Nuxt Dashboard` 
-    : 'ATC - Nuxt Dashboard',
+  titleTemplate: (title) =>
+    title ? `${title} | ATC - Nuxt Dashboard` : "ATC - Nuxt Dashboard",
   htmlAttrs: {
-    lang: 'ru'
+    lang: "ru",
   },
   bodyAttrs: {
-    class: 'relative font-normal font-outfit z-1 bg-gray-50 dark:bg-gray-900'
-  }
-})
+    class: "relative font-normal font-outfit z-1 bg-gray-50 dark:bg-gray-900",
+  },
+});
 </script>
